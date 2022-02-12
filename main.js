a5a468391e7dce5b11b51f478d30bb4dc4195213
StuffThing132 <76539609+StuffThing132@users.noreply.github.com>
2022-02-11 16:54:01 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+rightWristX = "";
+rightWristY = "";
+scorerightwrist = "";
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent('canvas');
+	video=createCapture(VIDEO);
+	video.size(600,300);
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+
+}
+
+function modelLoaded(){
+	console.log('Model Loaded!');
+}
+
+function draw() {
+	game()
+
+}
+
+
+
+
+
+
